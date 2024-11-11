14e7b491da69027e7a5075b543e4d01bee5b7d44
bleilaniperez <bleilaniperez@gmail.com>
2024-11-11 13:35:02 -0600

    Frontend for Login and Register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomePage from './HomePage';
+import Register from './Register';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <HomePage /> 
     </div>
   );
 }
